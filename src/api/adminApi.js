7461f6271a1fb95aa5dfab8efed0528274001018
wanjun <wanjunlan@gmail.com>
2024-02-01 14:52:10 -0800

    use md5 for password cryptography to implement Login function get request to make sure login successfully

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -1,7 +1,9 @@
-import axios from '../utils/request'
+import axios from '../utils/request';
+import md5 from 'md5';
 
 // Login function
 export const $login = async (params) => {
+  params.loginPwd = md5(md5(params.loginPwd).split('').reverse().join(''))
   let {data} = await axios.get('Admin/Login', {params})
   console.log(data);
 }

c711e487845c36a097c1b8874ff403ed2ea57412
wanjun <wanjunlan@gmail.com>
2024-02-01 11:55:45 -0800

    created a form for login and formatted

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,7 +1,75 @@
-import React from 'react';
+import React from "react";
+import "./Login.scss";
+import { Button, Form, Input } from "antd";
 
 export default function Login() {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <div className="content">
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+
+      </div>
+    </div>
+  );
 }

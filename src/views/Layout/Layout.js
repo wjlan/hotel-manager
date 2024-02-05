0366e9e70eacd99e09120ac7c83b5cfdaeb028cf
wanjun <wanjunlan@gmail.com>
2024-02-04 16:58:36 -0800

    Adjusted the overall layout in detail

--- src/views/Layout/Layout.js
+++ src/views/Layout/Layout.js
@@ -4,66 +4,55 @@ import {
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
-import React, { useState } from 'react';
+} from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import React, { useState } from "react";
 const { Header, Sider, Content } = Layout;
-import './Layout.scss'
-export default function() {
-  const [ collapsed, setCollapsed ] = useState(false);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+import "./Layout.scss";
+export default function () {
+  // side layout collapsed status
+  const [collapsed, setCollapsed] = useState(false);
   return (
-    <Layout className='layout'>
+    <Layout className="layout">
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo">{collapsed? 'Wanjun': 'Hotel Managerment System'}</div>
+        <div className="logo">
+          {collapsed ? "Wanjun" : "Hotel Managerment System"}
+        </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={["1"]}
           items={[
             {
-              key: '1',
+              key: "1",
               icon: <UserOutlined />,
-              label: 'nav 1',
+              label: "Account Management",
             },
             {
-              key: '2',
+              key: "2",
               icon: <VideoCameraOutlined />,
-              label: 'nav 2',
+              label: "Room Management",
             },
             {
-              key: '3',
+              key: "3",
               icon: <UploadOutlined />,
-              label: 'nav 3',
+              label: "Client Management",
             },
           ]}
         />
       </Sider>
-      <Layout className='site-layout'>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger',
-            onClick: () => setCollapsed(!collapsed),
-          })}
+      <Layout className="right">
+        <Header className="header">
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )}
         </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          Content
-        </Content>
+        <Content className="content">Content</Content>
       </Layout>
     </Layout>
   );
-};
+}

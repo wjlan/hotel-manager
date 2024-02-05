b143b9482ab7d6d407a44451cdb2f6a23fdc0eec
wanjun <wanjunlan@gmail.com>
2024-02-05 11:20:06 -0800

    Added menu icons in header and adjusted the layout

--- src/views/Layout/Layout.js
+++ src/views/Layout/Layout.js
@@ -1,15 +1,57 @@
+// icon
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu, theme } from "antd";
+import { Layout, Menu } from "antd";
 import React, { useState } from "react";
 const { Header, Sider, Content } = Layout;
 import "./Layout.scss";
 export default function () {
+  const [current, setCurrent] = useState("mail");
+  // Menu on the top
+  const items = [
+    {
+      label: "Home",
+      key: "home",
+      icon: <MailOutlined />,
+    },
+    {
+      label: "Mail",
+      key: "mail",
+      icon: <MailOutlined />,
+    },
+    {
+      label: "Notion",
+      key: "noti",
+      icon: <MailOutlined />,
+    },
+    {
+      label: "Account",
+      key: "SubMenu",
+      icon: <SettingOutlined />,
+      children: [
+        {
+          key: "1",
+          label: "Personal Information",
+        },
+        {
+          key: "2",
+          label: "Password reset",
+        },
+        {
+          key: "3",
+          label: "Logout",
+        },
+      ],
+    },
+  ];
   // side layout collapsed status
   const [collapsed, setCollapsed] = useState(false);
   return (
@@ -50,6 +92,13 @@ export default function () {
               onClick: () => setCollapsed(!collapsed),
             }
           )}
+          <Menu
+            theme="dark"
+            className="menu"
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={items}
+          />
         </Header>
         <Content className="content">Content</Content>
       </Layout>

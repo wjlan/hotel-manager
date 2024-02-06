706050ef3d640851628fb1b02d9230889e962942
wanjun <wanjunlan@gmail.com>
2024-02-06 13:07:34 -0800

    introduced a form to submit new role data

--- src/views/Role/Role.js
+++ src/views/Role/Role.js
@@ -1,41 +1,45 @@
-import React, {useEffect, useState} from 'react'
-import {Table, Button, Drawer} from 'antd'
-import {$list} from '../../api/RoleApi'
+import React, { useEffect, useState } from "react";
+import { Table, Button, Drawer, Form, Input } from "antd";
+import { $list } from "../../api/RoleApi";
 
 export default function Role() {
   // check drawer open or not
   const [open, setOpen] = useState(false);
   // close drawer function
   const onClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
+  // form submit function
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
   // roleList data
-  let [roleList, setRoleList] = useState([])
-  useEffect(()=>{
-    $list().then(data=>{
-      data = data.map(r=>{
+  let [roleList, setRoleList] = useState([]);
+  useEffect(() => {
+    $list().then((data) => {
+      data = data.map((r) => {
         return {
-          ...r, 
-          key:r.roleId
-        }
-      })
-      setRoleList(data)
-    })
-  }, [])
+          ...r,
+          key: r.roleId,
+        };
+      });
+      setRoleList(data);
+    });
+  }, []);
   const columns = [
     {
-      title: 'Role Id',
-      dataIndex: 'roldId',
+      title: "Role Id",
+      dataIndex: "roldId",
     },
     {
-      title: 'Role Name',
-      dataIndex: 'roleName',
+      title: "Role Name",
+      dataIndex: "roleName",
     },
   ];
   return (
     <>
-      <div className='search'>
-      <Button
+      <div className="search">
+        <Button
           size="small"
           onClick={() => {
             setOpen(true);
@@ -44,12 +48,54 @@ export default function Role() {
           Add
         </Button>
       </div>
-      <Table size='small' dataSource={roleList} columns={columns} />
-      <Drawer title="Add Role" width={500} onClose={onClose} open={open}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Table size="small" dataSource={roleList} columns={columns} />
+      <Drawer
+        title="Add Role"
+        width={500}
+        onClose={onClose}
+        open={open}
+      >
+        <Form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Role Name"
+      name="username"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+    <Form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
       </Drawer>
     </>
-  )
+  );
 }

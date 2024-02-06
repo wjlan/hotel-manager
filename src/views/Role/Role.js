8bac7da44c35b7cc32236d87d4599deb20c2846c
wanjun <wanjunlan@gmail.com>
2024-02-06 12:55:23 -0800

    Added a Drawer page for adding roles operation

--- src/views/Role/Role.js
+++ src/views/Role/Role.js
@@ -1,8 +1,14 @@
 import React, {useEffect, useState} from 'react'
-import {Table} from 'antd'
+import {Table, Button, Drawer} from 'antd'
 import {$list} from '../../api/RoleApi'
 
 export default function Role() {
+  // check drawer open or not
+  const [open, setOpen] = useState(false);
+  // close drawer function
+  const onClose = () => {
+    setOpen(false)
+  }
   // roleList data
   let [roleList, setRoleList] = useState([])
   useEffect(()=>{
@@ -28,7 +34,22 @@ export default function Role() {
   ];
   return (
     <>
+      <div className='search'>
+      <Button
+          size="small"
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          Add
+        </Button>
+      </div>
       <Table size='small' dataSource={roleList} columns={columns} />
+      <Drawer title="Add Role" width={500} onClose={onClose} open={open}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
     </>
   )
 }

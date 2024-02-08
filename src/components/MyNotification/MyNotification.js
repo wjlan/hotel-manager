cffb0e20ec0867562000c3e60226200d32e5ed5a
wanjun <wanjunlan@gmail.com>
2024-02-07 16:39:55 -0800

    minor debug for notification

--- src/components/MyNotification/MyNotification.js
+++ src/components/MyNotification/MyNotification.js
@@ -1,12 +1,12 @@
 import React, {useEffect} from 'react'
 import {notification} from 'antd'
 
-export default function MyNotification(notiMsg) {
+export default function MyNotification({notiMsg}) {
   const [api, contextHolder] = notification.useNotification();
   useEffect(()=>{
     // if type is valid, open the notification
     if(notiMsg.type){
-      api[notiMsg.description]({
+      api[notiMsg.type]({
         message: 'System notification',
         description: notiMsg.description
       })

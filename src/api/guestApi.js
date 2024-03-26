b7a5eb1ec52617189a6061c2cd864df5618101a8
wanjun <wanjunlan@gmail.com>
2024-03-26 16:47:03 -0700

    connect to guest api

---
+++ src/api/guestApi.js
@@ -0,0 +1,32 @@
+import axios from '../utils/request'
+
+// Guest list
+export const $list = async (params)=>{
+  let {data} = await axios.get('GuestRecord/List',{params})
+  return data
+}
+
+// Get single one guest
+export const $getOne = async(params)=>{
+  let {data} = await axios.get('GuestRecord/GetOne',{params})
+  return data
+}
+
+// Add guest
+export const $add = async (params)=>{
+  let {data} = await axios.post('GuestRecord/Add',params)
+  return data
+}
+
+// Edit guest
+export const $update = async (params)=>{
+  let {data} = await axios.post('GuestRecord/Update',params)
+  return data
+}
+
+// Delete guest
+export const $del = async (params)=>{
+  let {data} = await axios.post('GuestRecord/Delete',params)
+  return data
+}
+
